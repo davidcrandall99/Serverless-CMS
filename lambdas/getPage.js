c966d8a837cd1a34eae31ae297031be8df296ae4
dcrandall <davidcrandall@dcrandall-mbp15.local>
2019-05-12 18:12:23 -0700

    Added basic functions with some validation
    Specifically, I’ve added a validation against if required fields are
    present in the query parameters, and that they have data in them.
    Further validation TBA.

--- lambdas/getPage.js
+++ lambdas/getPage.js
@@ -1,7 +1,11 @@
 'use strict';
 
-
 //function to get page
-module.exports.handler = (e) => {
-    return e;
+module.exports.handler = (event, context, callback) => {
+    let response = {
+        "statusCode"    : 200,
+        "headers"       : {},
+        "body"          : JSON.stringify(event.path)
+    }
+    callback(null, response);
 }
--- lambdas/writePage.js
+++ lambdas/writePage.js
@@ -1,3 +1,43 @@
-module.exports.handler = (e) => {
-	return e;
+'use strict';
+/*
+
+TO DO:
+- function to write data
+- validate data
+	fail if not url encoded uri
+	fail if author does not exist in author db
+	fail if mr author does not exist in author db
+	fail if category_id is empty
+	fail if category_id is not in categories db, or create the category (TBD)
+	fail if any types are incorrect
+- only fire write-data function when data is validated
+
+*/
+
+
+class Validator {
+	//validate required fields
+	requiredFields(data) {
+		//these fields must e
+		const fields = ["uri", "or_author", "mr_author", "title", "category_id", "content", "template_id"];
+		var i;
+		for (i = 0; i<fields.length; i++) {
+			if(data[fields[i]]) {
+				if(i === fields.length - 1) {
+					return data;
+				}
+			} else {
+				return {"error" : `${fields[i]} is required`}
+			}
+		}
+
+	}
+
+}
+let validate = new Validator;
+
+
+module.exports.handler = (event, context, callback) => {
+	let data = event.queryStringParameters;
+	console.log(validate.requiredFields(data));
 }
